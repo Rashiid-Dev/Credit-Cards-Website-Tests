6b45dc82fff6a8f5412161036b44370302f2d72b
Abdirashiid Jama <rashiid_jacfar@hotmail.com>
2020-04-16 17:27:39 +0100

    Created CreditCardApplicationShould.cs and a test that tests returns when clicking on the apply now button in the slider

---
+++ CreditCards.UITests/CreditCardApplicationShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests
+{
+    [Trait("Category", "Applications")]
+    public class CreditCardApplicationShould
+    {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string ApplyUrl = "http://localhost:44108/Apply";
+
+        [Fact]
+        public void BeInitiatedFromHomePage_NewLowRate()
+        {
+            using(IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                IWebElement applyLink = driver.FindElement(By.Name("ApplyLowRate"));
+
+                applyLink.Click();
+
+                Assert.Equal(driver.Url, ApplyUrl);
+
+                Assert.Equal("Credit Card Application - Credit Cards", driver.Title);
+            }
+        }
+    }
+}
