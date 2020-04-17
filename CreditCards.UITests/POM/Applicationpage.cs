2eeab09fc39a3bbc8f6b2fe5b47219089a098663
Abdirashiid Jama <rashiid_jacfar@hotmail.com>
2020-04-17 14:40:57 +0100

    Created ApplicationPage.cs in POM

---
+++ CreditCards.UITests/POM/Applicationpage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CreditCards.UITests.POM
+{
+    class ApplicationPage
+    {
+        private readonly IWebDriver Driver;
+        private const string PageUrl = "http://localhost:44108/Apply";
+        private const string PageTitle = "Credit Card Application - Credit Cards";
+
+        public ApplicationPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void NavigateTo()
+        {
+            Driver.Navigate().GoToUrl(PageUrl);
+            EnsurePageLoaded();
+        }
+
+        public void EnsurePageLoaded(bool onlyCheckUrlStartsWithExpectedText = true)
+        {
+            bool urlIsCorrect;
+
+            if (onlyCheckUrlStartsWithExpectedText)
+            {
+                urlIsCorrect = Driver.Url.StartsWith(PageUrl);
+            }
+            else
+            {
+                urlIsCorrect = Driver.Url == PageUrl;
+            }
+
+            bool pageHasLoaded = urlIsCorrect && (Driver.Title == PageTitle);
+
+            if (!pageHasLoaded)
+            {
+                throw new Exception($"Failed to load page. Page URL = '{Driver.Url}' Page Source: \r\n {Driver.PageSource}");
+            }
+        }
+    }
+}
